82069f530263fde11e0e6980f26c11973352f906
Helge Hess <devteam@zeezide.de>
2017-09-29 16:41:57 +0200

    Update shims to point to Xcode 9 release
    We always refer to /Applications/Xcode.app/ in this
    specific SPM package.

--- CSQLite3Shim.h
+++ CSQLite3Shim.h
@@ -2,10 +2,16 @@
 #define __CSQLITE3_SHIM_H__
 
 #if defined(__APPLE__) && defined(__MACH__)
-#  if __clang_major__ >= 9 // until the release ...
-#    include "/Applications/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
-#  else
+#  if __clang_major__ >= 9 // released now. assume Xcode 9.
 #    include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
+#  else
+#    if 0
+#      include "/Applications/Xcode 8.3.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
+#      include "/usr/include/sqlite3.h"
+#    else
+//     I guess we always want this in the SPM package.
+#      include "/Applications/Xcode/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sqlite3.h"
+#    endif
 #  endif
 #else
 #  include "/usr/include/sqlite3.h"
