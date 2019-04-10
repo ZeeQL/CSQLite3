3c68858785fb11a487e43328802d605d1d6fcd3a
Helge Heß <devteam@zeezide.de>
2019-04-10 18:46:29 +0200

    Attempt to do a modern target
    ...

---
+++ Package@swift-5.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "CSQLite3",
+    products: [
+        .library(name: "CSQLite3", targets: ["CSQLite3"]),
+    ],
+    targets: [
+        .systemLibrary(name: "CSQLite3", path: ".")
+    ]
+)
